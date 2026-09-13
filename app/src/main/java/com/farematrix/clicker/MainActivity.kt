7c6202b0cd88ee67d861ea77b6ef5c2cac1ee545
Aizen807 <2.09431518e+08+Aizen807@users.noreply.github.com>
2026-09-13 12:59:53 +0000

    Fix coroutine cancellation import

--- app/src/main/java/com/farematrix/clicker/MainActivity.kt
+++ app/src/main/java/com/farematrix/clicker/MainActivity.kt
@@ -17,6 +17,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.math.abs
